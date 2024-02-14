3a37e5632315b91caec664fe9db7b82d0a6f61bb
mujanati13 <simojanati.1j@gmail.com>
2024-02-14 19:20:29 +0100

    commit

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var rateLimit = require("express-rate-limit");
 
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// Define rate limiting options
+var limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+// Apply rate limiter to all requests
+app.use(limiter);
 
 app.use(logger('dev'));
 app.use(express.json());
